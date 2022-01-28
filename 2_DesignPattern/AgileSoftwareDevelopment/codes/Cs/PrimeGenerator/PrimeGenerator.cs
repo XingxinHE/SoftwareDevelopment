663713ce4c20e49d4ae31bde25e62f837b977c4f
Xingxin HE <xingxin.he@mail.polimi.it>
2022-01-28 12:22:16 +0800

    PrimerGenerator_ver3

--- 2_DesignPattern/AgileSoftwareDevelopment/codes/Cs/PrimeGenerator/PrimeGenerator.cs
+++ 2_DesignPattern/AgileSoftwareDevelopment/codes/Cs/PrimeGenerator/PrimeGenerator.cs
@@ -13,18 +13,17 @@ namespace PrimeGenerator
          * https://zh.wikipedia.org/wiki/%E5%9F%83%E6%8B%89%E6%89%98%E6%96%AF%E7%89%B9%E5%B0%BC%E7%AD%9B%E6%B3%95
          */
 
-        private static int s;
         private static bool[] f;
-        private static int[] primes;
+        private static int[] result;
 
-        private static void loadPrimes()
+        private static void putUncrossedIntegerIntoResult()
         {
             int i;
             int j;
 
             // how many primes are there?
             int count = 0;
-            for (i = 0; i < s; i++)
+            for (i = 0; i < f.Length; i++)
             {
                 if (f[i])
                 {
@@ -32,29 +31,29 @@ namespace PrimeGenerator
                 }
             }
 
-            primes = new int[count];
+            result = new int[count];
 
             //move the primes into the result
-            for (i = 0, j = 0; i < s; i++)
+            for (i = 0, j = 0; i < f.Length; i++)
             {
                 if (f[i])
                 {
-                    primes[j] = i;
+                    result[j] = i;
                     j++;  // if it is prime, increment
                 }
             }
         }
 
-        private static void sieve()
+        private static void crossOutMultiples()
         {
             int i;
             int j;
             // here is important, see the wiki
-            for (i = 2; i < Math.Sqrt(s) + 1; i++)
+            for (i = 2; i < Math.Sqrt(f.Length) + 1; i++)
             {
                 if (f[i])
                 {
-                    for (j = 2*i; j <s ; j+=i)
+                    for (j = 2 * i; j < f.Length; j += i)
                     {
                         f[j] = false;
                     }
@@ -62,22 +61,21 @@ namespace PrimeGenerator
             }
         }
 
-        private static void initializeSieve(int maxValue)
+        private static void initializeArrayOfIntegers(int maxValue)
         {
             // since start counting from 0 to last index, therefore +1
             // e.g. max=25, we have to loop from 0 to 25
-            s = maxValue + 1;
-            f = new bool[s];
-            int i;
-
-            // initialize array to be true
-            for (i = 0;i<s; i++)
-            {
-                f[i] = true;
-            }
+            f = new bool[maxValue + 1];
 
             // 0 & 1 are known non-primes
             f[0] = f[1] = false;
+
+            int i;
+            // initialize array to be true
+            for (i = 2; i < f.Length; i++)
+            {
+                f[i] = true;
+            }
         }
 
         public static int[] GeneratePrimes(int maxValue)
@@ -88,10 +86,10 @@ namespace PrimeGenerator
             }
             else
             {
-                initializeSieve(maxValue);
-                sieve();
-                loadPrimes();
-                return primes;  //return the prime
+                initializeArrayOfIntegers(maxValue);
+                crossOutMultiples();
+                putUncrossedIntegerIntoResult();
+                return result;  //return the prime
             }
         }
     }
