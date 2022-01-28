dfc6d57b4a69607e6f199d8e3f5d149f3d1b0b68
Xingxin HE <xingxin.he@mail.polimi.it>
2022-01-28 12:42:59 +0800

    PrimeGenerator_ver4

--- 2_DesignPattern/AgileSoftwareDevelopment/codes/Cs/PrimeGenerator/PrimeGenerator.cs
+++ 2_DesignPattern/AgileSoftwareDevelopment/codes/Cs/PrimeGenerator/PrimeGenerator.cs
@@ -13,7 +13,7 @@ namespace PrimeGenerator
          * https://zh.wikipedia.org/wiki/%E5%9F%83%E6%8B%89%E6%89%98%E6%96%AF%E7%89%B9%E5%B0%BC%E7%AD%9B%E6%B3%95
          */
 
-        private static bool[] f;
+        private static bool[] isCrossed;
         private static int[] result;
 
         private static void putUncrossedIntegerIntoResult()
@@ -23,9 +23,9 @@ namespace PrimeGenerator
 
             // how many primes are there?
             int count = 0;
-            for (i = 0; i < f.Length; i++)
+            for (i = 0; i < isCrossed.Length; i++)
             {
-                if (f[i])
+                if (isCrossed[i])
                 {
                     count++;
                 }
@@ -34,9 +34,9 @@ namespace PrimeGenerator
             result = new int[count];
 
             //move the primes into the result
-            for (i = 0, j = 0; i < f.Length; i++)
+            for (i = 0, j = 0; i < isCrossed.Length; i++)
             {
-                if (f[i])
+                if (isCrossed[i])
                 {
                     result[j] = i;
                     j++;  // if it is prime, increment
@@ -44,37 +44,53 @@ namespace PrimeGenerator
             }
         }
 
+        private static int calcMaxPrimeFactor()
+        {
+            // We cross out all multiples of p; where p is prime.
+            // Thus, all crossed out multiples have p and q for
+            // factors. If p > sqrt of the size of the array, then
+            // q will never be greater than 1. Thus p is the
+            // largest prime factor in the array, and is also
+            // the iteration limit.
+
+            double maxPrimeFactor = Math.Sqrt(isCrossed.Length) + 1;
+            return (int)maxPrimeFactor;
+        }
+
         private static void crossOutMultiples()
         {
-            int i;
-            int j;
-            // here is important, see the wiki
-            for (i = 2; i < Math.Sqrt(f.Length) + 1; i++)
+            int maxPrimeFactor = calcMaxPrimeFactor();
+            for (int i = 2; i<=maxPrimeFactor; i++)
             {
-                if (f[i])
+                if (notCrossed(i))
                 {
-                    for (j = 2 * i; j < f.Length; j += i)
-                    {
-                        f[j] = false;
-                    }
+                    crossOutMultiplesOf(i);
                 }
             }
+        }
+
+        private static void crossOutMultiplesOf(int i)
+        {
+            for (int multiple = 2*i; multiple < isCrossed.Length; multiple+=i)
+            {
+                isCrossed[multiple] = true;
+            }
+        }
+
+        private static bool notCrossed(int i)
+        {
+            return isCrossed[i] == false;
         }
 
         private static void initializeArrayOfIntegers(int maxValue)
         {
             // since start counting from 0 to last index, therefore +1
             // e.g. max=25, we have to loop from 0 to 25
-            f = new bool[maxValue + 1];
+            isCrossed = new bool[maxValue + 1];
 
-            // 0 & 1 are known non-primes
-            f[0] = f[1] = false;
-
-            int i;
-            // initialize array to be true
-            for (i = 2; i < f.Length; i++)
+            for (int i = 2 ; i < isCrossed.Length; i++)
             {
-                f[i] = true;
+                isCrossed [i] = false;
             }
         }
 
